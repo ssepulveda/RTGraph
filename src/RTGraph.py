54e09f5a707ab2365dd56bb64681088c33bfa0fe
Sebastián Sepúlveda <ssepulveda.sm@gmail.com>
2016-03-20 22:00:37 -0700

    added basic pyqtgraph plotting

--- src/RTGraph.py
+++ src/RTGraph.py
@@ -1,14 +1,18 @@
 import multiprocessing
 import sys
 import platform
+
 import logging as log
 import logging.handlers
 import argparse
+
 from serialProcess import SerialProcess
+from ringBuffer import RingBuffer
 from gui import *
 
 
 TIMEOUT = 1000
+SAMPLES = 100
 
 
 class MainWindow(QtGui.QMainWindow):
@@ -17,28 +21,79 @@ class MainWindow(QtGui.QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
+        self.plt1 = None
+        self.timer_plot_update = None
+        self.timer_freq_update = None
+        self.data = None
+        self.time = None
+        self.sp = None
 
-def main():
-    result_queue = multiprocessing.Queue()
-    sp = SerialProcess(result_queue)
-    ports = sp.get_ports()
-    log.info(ports)
-    if 0 < len(ports):
-        sp.open_port(ports[0])
-        if sp.is_port_available(ports[0]):
-            sp.start()
-            value = result_queue.get(block=True, timeout=TIMEOUT)
-            count = 0
-            while count < 5:
-                if not result_queue.empty():
-                    value = result_queue.get(block=False)
-                    count = value[1]
-            sp.stop()
-            sp.join()
+        self.queue = multiprocessing.Queue()
+        self.reset_buffers()
+
+        # configures
+        self.configure_plot()
+        self.configure_timers()
+        self.configure_signals()
+
+    def configure_plot(self):
+        self.ui.plt.setBackground(background=None)
+        self.ui.plt.setAntialiasing(True)
+        self.plt1 = self.ui.plt.addPlot(row=1, col=1)
+
+    def configure_timers(self):
+        self.timer_plot_update = QtCore.QTimer(self)
+        # self.timer_freq_update = QtCore.QTimer(self)
+        QtCore.QObject.connect(self.timer_plot_update,
+                               QtCore.SIGNAL('timeout()'), self.update_plot)
+        # QtCore.QObject.connect(self.timer_freq_update,
+        #                       QtCore.SIGNAL('timeout()'), self.update_freq)
+
+    def configure_signals(self):
+        QtCore.QObject.connect(self.ui.pButton_Start,
+                               QtCore.SIGNAL('clicked()'), self.start)
+        QtCore.QObject.connect(self.ui.pButton_Stop,
+                               QtCore.SIGNAL('clicked()'), self.stop)
+
+    def reset_buffers(self):
+            self.data = RingBuffer(SAMPLES)
+            self.time = RingBuffer(SAMPLES)
+            while not self.queue.empty():
+                self.queue.get()
+            log.info("Buffers cleared")
+
+    def update_plot(self):
+        log.debug("Updating plot")
+        while not self.queue.empty():
+            data = self.queue.get(False)
+            value = str(data[0]).split(',')
+            self.data.append(float(value[1]))
+            self.time.append(data[1])
+
+        self.plt1.clear()
+        self.plt1.plot(x=self.time.get_all(), y=self.data.get_all(), pen='#2196F3')
+
+    def start(self):
+        log.info("Clicked start")
+        self.sp = SerialProcess(self.queue)
+        ports = self.sp.get_ports()
+        log.info(ports)
+        if 0 < len(ports):
+            self.sp.open_port(ports[0])
+            if self.sp.is_port_available(ports[0]):
+                self.sp.start()
+                self.timer_plot_update.start(10)
+            else:
+                log.info("Port is not available")
         else:
-            log.info("Port is not available")
-    else:
-        log.warning("No ports detected")
+            log.warning("No ports detected")
+
+    def stop(self):
+        log.info("Clicked stop")
+        self.timer_plot_update.stop()
+        self.sp.stop()
+        self.sp.join()
+        self.reset_buffers()
 
 
 def start_logging(level):
@@ -81,7 +136,6 @@ if __name__ == '__main__':
 
     log.info("Starting RTGraph")
 
-    # main()
     app = QtGui.QApplication(sys.argv)
     win = MainWindow()
     win.show()
