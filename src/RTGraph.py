bb6ff0cfe0b03be87b15de0aa38ff514e5c7328f
Sebastian Sepulveda <ssepulveda.sm@gmail.com>
2016-12-10 22:36:28 -0300

    Checking python version before running app

--- src/RTGraph.py
+++ src/RTGraph.py
@@ -10,27 +10,27 @@ MINIMAL_PYTHON_VERSION_MINOR = 2
 
 
 if __name__ == '__main__':
-    if Architecture.is_python_version(MINIMAL_PYTHON_VERSION_MAJOR, minor=MINIMAL_PYTHON_VERSION_MINOR):
-        multiprocessing.freeze_support()
-        args = ArgParser()
-        args.create()
-        args.parse()
+    multiprocessing.freeze_support()
+    args = ArgParser()
+    args.create()
+    args.parse()
 
+    app = QtGui.QApplication(sys.argv)
+    if Architecture.is_python_version(MINIMAL_PYTHON_VERSION_MAJOR, minor=MINIMAL_PYTHON_VERSION_MINOR):
         log.info("Starting RTGraph")
-        app = QtGui.QApplication(sys.argv)
         win = mainWindow.MainWindow(port=args.get_user_port(),
                                     bd=args.get_user_bd(),
-                                    samples=args.get_user_samples()
-                                    )
+                                    samples=args.get_user_samples())
         win.show()
         app.exec()
 
         log.info("Finishing RTGraph\n")
-        log.shutdown()
         win.close()
-        app.exit()
-        sys.exit()
     else:
-        print("RTGraph requires Python {}.{} to run"
-              .format(MINIMAL_PYTHON_VERSION_MAJOR, MINIMAL_PYTHON_VERSION_MINOR))
-        exit(1)
+        txt = str("RTGraph requires Python {}.{} to run"
+                  .format(MINIMAL_PYTHON_VERSION_MAJOR, MINIMAL_PYTHON_VERSION_MINOR))
+        log.error(txt)
+
+    app.exit()
+    log.shutdown()
+    sys.exit()
