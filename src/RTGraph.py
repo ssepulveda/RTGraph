09445d90047fb2dc324edfd840e75b72143eaa27
Sebastian Sepulveda <ssepulveda.sm@gmail.com>
2016-12-19 08:11:49 -0400

    modularized

--- src/RTGraph.py
+++ src/RTGraph.py
@@ -9,28 +9,47 @@ MINIMAL_PYTHON_VERSION_MAJOR = 3
 MINIMAL_PYTHON_VERSION_MINOR = 2
 
 
-if __name__ == '__main__':
-    multiprocessing.freeze_support()
+def _init_logger():
     args = Arguments()
     args.create()
     args.set_user_log_level()
+    return args
+
+
+def _start_app(app, args):
+    log.info("Starting RTGraph")
+    win = mainWindow.MainWindow(port=args.get_user_port(),
+                                bd=args.get_user_bd(),
+                                samples=args.get_user_samples())
+    win.show()
+    app.exec()
+
+    log.info("Finishing RTGraph\n")
+    win.close()
+
+
+def _fail_app():
+    txt = str("RTGraph requires Python {}.{} to run"
+              .format(MINIMAL_PYTHON_VERSION_MAJOR, MINIMAL_PYTHON_VERSION_MINOR))
+    log.error(txt)
+
+
+def _close_app():
+    log.shutdown()
+    sys.exit()
+
+
+if __name__ == '__main__':
+    multiprocessing.freeze_support()
+    args = _init_logger()
 
     app = QtGui.QApplication(sys.argv)
     if Architecture.is_python_version(MINIMAL_PYTHON_VERSION_MAJOR, minor=MINIMAL_PYTHON_VERSION_MINOR):
-        log.info("Starting RTGraph")
-        win = mainWindow.MainWindow(port=args.get_user_port(),
-                                    bd=args.get_user_bd(),
-                                    samples=args.get_user_samples())
-        win.show()
-        app.exec()
-
-        log.info("Finishing RTGraph\n")
-        win.close()
+        _start_app(app, args)
     else:
-        txt = str("RTGraph requires Python {}.{} to run"
-                  .format(MINIMAL_PYTHON_VERSION_MAJOR, MINIMAL_PYTHON_VERSION_MINOR))
-        log.error(txt)
-
+        _fail_app()
     app.exit()
-    log.shutdown()
-    sys.exit()
+    _close_app()
+
+
+
