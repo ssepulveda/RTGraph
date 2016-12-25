3d2772fe735a3de33ffc0d0b9fd26d568891266c
Sebastian Sepulveda <ssepulveda.sm@gmail.com>
2016-12-25 12:52:26 -0300

    WIP: worker unit test

---
+++ rtgraph/tests/workerTests.py
@@ -0,0 +1,42 @@
+import unittest
+from time import sleep
+
+from rtgraph.core.worker import Worker
+from rtgraph.core.constants import SourceType
+
+
+class WorkerTests(unittest.TestCase):
+    def test_one(self):
+        time = 2
+        speed = 0.02
+        error = 0.8
+        zeros = 0
+        passed = False
+        expected_samples = (time * (1/speed))
+        samples_plus_error = (expected_samples / error)
+
+        # Log(LoggerLevel.DEBUG, enable_console=True)
+        worker = Worker(port=None,
+                        speed=float(speed),
+                        samples=int(samples_plus_error),
+                        source=SourceType.simulator,
+                        export_enabled=False)
+        worker.start()
+        while time > 0:
+            while not worker.queue.empty():
+                worker.add_values(worker.queue.get(False)[1])
+            sleep(speed)
+            time -= speed
+        worker.stop()
+
+        for v in worker.get_values_buffer(0):
+            if v == 0:
+                zeros += 1
+
+        if 0 < zeros <= (samples_plus_error - expected_samples):
+            passed = True
+        self.assertTrue(passed)
+
+
+if __name__ == '__main__':
+    unittest.main()
